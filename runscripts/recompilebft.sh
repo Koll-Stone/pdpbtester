3144479817e31ad7412964c7f2d43344beff3fa2
Ubuntu <ubuntu@ip-172-31-37-242.ap-northeast-2.compute.internal>
2023-02-04 11:54:10 +0000

    upload all

---
+++ runscripts/recompilebft.sh
@@ -0,0 +1,14 @@
+echo "######start..."
+
+echo "######downloading bft smart..."
+cd /home/ubuntu/xacmlProject
+rm -rf storage
+git clone https://github.com/Koll-Stone/storage.git
+
+echo "######compile bftsmart to maven..."
+cp /home/ubuntu/xacmlProject/storage/BFT-SMaRt.jar /home/ubuntu/xacmlProject/pdpbtester/lib
+mvn install:install-file -Dfile=lib/BFT-SMaRt.jar -DgroupId=org.ulisboa -DartifactId=bftsmart -Dpackaging=jar -Dversion=1.0
+mvn dependency:build-classpath -Dmdep.outputFile=runscripts/cpcontent
+echo ":$(pwd)/target/classes" >> runscripts/cpcontent
+
+echo "######finished..."
