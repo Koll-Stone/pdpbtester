4ab88f5a123d623d0dfe2d5ce86d1b41393a82f8
Ubuntu <ubuntu@ip-172-31-37-242.ap-northeast-2.compute.internal>
2023-02-10 03:38:34 +0000

    update sh

--- runscripts/recompilebft.sh
+++ runscripts/recompilebft.sh
@@ -9,7 +9,7 @@ git pull
 echo "######compile bftsmart to maven..."
 cp /home/ubuntu/xacmlProject/storage/BFT-SMaRt.jar /home/ubuntu/xacmlProject/pdpbtester/lib
 cd /home/ubuntu/xacmlProject/pdpbtester
-mvn install:install-file -Dfile=lib/BFT-SMaRt.jar -DgroupId=org.ulisboa -DartifactId=bftsmart -Dpackaging=jar -Dversion=1.0
+mvn clean install:install-file -Dfile=lib/BFT-SMaRt.jar -DgroupId=org.ulisboa -DartifactId=bftsmart -Dpackaging=jar -Dversion=1.0
 mvn dependency:build-classpath -Dmdep.outputFile=runscripts/cpcontent
 echo ":$(pwd)/target/classes" >> runscripts/cpcontent
 
