38ceb6d2068a4e40b6df7dfd1058942fbc713733
Ubuntu <ubuntu@ip-172-31-37-242.ap-northeast-2.compute.internal>
2023-02-15 06:55:53 +0000

    update ips

--- runscripts/unifiedrun.sh
+++ runscripts/unifiedrun.sh
@@ -27,13 +27,13 @@ if [ $flag -eq 1 ]
 then
     if [ $me -le 3 ]
     then
-        command="runscripts/myrun.sh org.example.testserver $me false"
+        command="runscripts/myrun.sh org.example.testserver $me true"
         echo $command
         bash $command
     else
         client=$(($me-4))
         # start=$(($(($client*600))+1001))
-        command="runscripts/myrun.sh org.example.testClient $client 300 201 false 20"
+        command="runscripts/myrun.sh org.example.testClient $client 300 201 true 20"
         echo $command
         bash $command
     fi   
