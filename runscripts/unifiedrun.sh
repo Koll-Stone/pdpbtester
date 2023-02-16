f53b6d4ad024aa22395babb402082f0374a1d30e
Ubuntu <ubuntu@ip-172-31-37-242.ap-northeast-2.compute.internal>
2023-02-16 09:50:14 +0000

    update ip

--- runscripts/unifiedrun.sh
+++ runscripts/unifiedrun.sh
@@ -34,7 +34,7 @@ then
     else
         client=$(($me-$servernum))
         # start=$(($(($client*1000))+1001))
-        command="runscripts/myrun.sh org.example.testClient $client 80 751 true 100 read"
+        command="runscripts/myrun.sh org.example.testClient $client 90 670 true 100 read"
         echo $command
         bash $command
     fi   
