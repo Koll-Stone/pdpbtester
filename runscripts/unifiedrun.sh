b89e657186bbad66608695deddde261efae4b660
Ubuntu <ubuntu@ip-172-31-39-79.ap-northeast-2.compute.internal>
2023-02-16 14:32:53 +0000

    test new parameter

--- runscripts/unifiedrun.sh
+++ runscripts/unifiedrun.sh
@@ -34,7 +34,7 @@ then
     else
         client=$(($me-$servernum))
         # start=$(($(($client*1000))+1001))
-        command="runscripts/myrun.sh org.example.testClient $client 60 1001 true 100 read"
+        command="runscripts/myrun.sh org.example.testClient $client 100 601 true 100 read"
         echo $command
         bash $command
     fi   
