0b3789f29d20e87cdf5c3f9c38ab60d33457ca66
Ubuntu <ubuntu@ip-172-31-39-185.ap-northeast-2.compute.internal>
2023-02-19 08:53:27 +0000

    update ip

--- runscripts/unifiedrun.sh
+++ runscripts/unifiedrun.sh
@@ -38,7 +38,7 @@ then
         then
             papnum=5
         fi
-        command="bash runscripts/myrun.sh org.example.testClient 0 $papnum $pepinitid 150 400 true 20"
+        command="runscripts/myrun.sh org.example.testClient 0 $papnum $pepinitid 150 400 true 20"
     fi   
     echo $command
     bash $command
