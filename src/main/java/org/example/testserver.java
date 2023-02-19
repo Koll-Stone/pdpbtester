34fd95f7df8c3de1982ca0968c1e3692d2407589
Ubuntu <ubuntu@ip-172-31-39-185.ap-northeast-2.compute.internal>
2023-02-19 10:01:30 +0000

    update ip

--- src/main/java/org/example/testserver.java
+++ src/main/java/org/example/testserver.java
@@ -3,15 +3,20 @@ package org.example;
 import bftsmart.tom.ServiceReplica;
 import bftsmart.tom.server.PDPB.POrder;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+
 public class testserver extends POrder {
     public testserver(int id, boolean signed) {
         xacmlExecutor xexecutor = new xacmlExecutor(signed);
 
         System.out.println("created xacml executor "+id);
-
+        
         setReplicaId(id);
         SetPExecutor(xexecutor);
         setSysStartTime(System.currentTimeMillis());
+        System.out.print("start point real time is: "+Instant.now().toString());
+
         xexecutor.setPOrder(this);
 
         ServiceReplica sr = new ServiceReplica(id, this, this);
