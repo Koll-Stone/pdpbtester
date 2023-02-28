d71525e6a28b41150070c96e6bc61e844a8f053f
Ubuntu <ubuntu@ip-172-31-42-245.ap-northeast-2.compute.internal>
2023-02-28 09:37:30 +0000

    update config

--- runscripts/unifiedrun.sh
+++ runscripts/unifiedrun.sh
@@ -45,7 +45,7 @@ then
             opnum=200
         fi
 
-        command="runscripts/myrun.sh org.example.testClient 0 $papnum $pepinitid 330 $opnum true 100"
+        command="runscripts/myrun.sh org.example.testClient 0 $papnum $pepinitid 500 $opnum true 100"
     fi   
     echo $command
     bash $command
